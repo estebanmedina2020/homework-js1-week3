3ae85f35833cd4105f5594b922394dc728b3fcb5
Esteban <emedina@creatonico.com>
2020-04-17 18:41:25 +0200

    finish triangle exercise

--- 1-triangle.js
+++ 1-triangle.js
@@ -16,4 +16,18 @@ console.log(abc.length);
 -> 3
 */
 
+/*
+let number = "#######";
+console.log(number.length);
+-> 7
+*/
+
 //your code here
+makeTriangle(7);
+function makeTriangle(altura) {
+  let triangle = [];
+  for (let i = 1; i <= altura; i++) {
+    triangle += "#";
+    console.log(triangle);
+  }
+}
